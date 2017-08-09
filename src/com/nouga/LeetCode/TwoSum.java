a3ee90380b9e42c73a0f1a3f1c1cd6cda3a1e6fc
NougaGuy <852681432@qq.com>
2017-08-09 14:52:58 +0800

    LeetCode

---
+++ src/com/nouga/LeetCode/TwoSum.java
@@ -0,0 +1,14 @@
+package com.nouga.LeetCode;
+
+/**
+ * @author Nouga Date : 2017/8/9 Declaration : All Rights Reserved !
+ */
+public class TwoSum {
+
+	/**
+	 * Give an array of integers , return indices of the two numbers such that
+	 * they add up to a specific target .
+	 * You may assume that each input would had exactly one solution
+	 * and you may not use the same element twice . 
+	 */
+}
